37d04f7ae919f33cb45d35e938f34dd4dd00ab47
vigeorgescu <vigeorgescu@pentalog.com>
2020-03-26 16:48:31 +0200

    tst

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -l
 
 echo "user: [${cc_user}] pass: [${cc_pass}]"
+git fetch --unshallow
 git remote add tempbranch https://${cc_user}:${cc_pass}@git-codecommit.eu-west-1.amazonaws.com/v1/repos/gigel
 git push -f tempbranch $(git rev-parse --abbrev-ref HEAD):master
